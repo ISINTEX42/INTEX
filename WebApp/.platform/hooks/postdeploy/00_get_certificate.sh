204ce333cca1af5c2c8a1ddaea1c7121f45aaaae
Braden2n <braden@toonetown.com>
2023-12-04 14:10:46 -0700

    Fix for multi-HTTPS Certs

--- WebApp/.platform/hooks/postdeploy/00_get_certificate.sh
+++ WebApp/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d mindfulmediasurvey.com -d www.mindfulmediasurvey.com -d mindfulmediasurvey.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email isintex42@gmail.com
+sudo certbot --expand -n -d mindfulmediasurvey.com,www.mindfulmediasurvey.com,mindfulmediasurvey.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email isintex42@gmail.com
