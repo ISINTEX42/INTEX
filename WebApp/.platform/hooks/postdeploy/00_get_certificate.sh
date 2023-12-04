6bce7b1031c1b6a173d0767a1544d2be3e1b949b
Braden2n <braden@toonetown.com>
2023-12-04 14:52:13 -0700

    Certbot

--- WebApp/.platform/hooks/postdeploy/00_get_certificate.sh
+++ WebApp/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -2,5 +2,4 @@
 sudo certbot delete --cert-name mindfulmediasurvey.com
 sudo certbot delete --cert-name www.mindfulmediasurvey.com
 sudo certbot delete --cert-name mindfulmediasurvey.us-east-1.elasticbeanstalk.com
-sudo certbot -n --expand -d mindfulmediasurvey.com,www.mindfulmediasurvey.com --nginx --agree-tos --email isintex42@gmail.com
-sudo certbot -n --expand -d mindfulmediasurvey.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email isintex42@gmail.com
+sudo certbot -n --expand -d mindfulmediasurvey.com --nginx --agree-tos --email isintex42@gmail.com
