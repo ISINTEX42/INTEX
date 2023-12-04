a239957aacd8114d9e320a4e8c0c949198725e75
Braden2n <braden@toonetown.com>
2023-12-04 14:36:40 -0700

    Certbot

--- WebApp/.platform/hooks/postdeploy/00_get_certificate.sh
+++ WebApp/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sudo certbot -n -d mindfulmediasurvey.com --nginx --agree-tos --email isintex42@gmail.com
-sudo certbot -n -d mindfulmediasurvey.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email isintex42@gmail.com
+sudo certbot -n --expand -d mindfulmediasurvey.com,www.mindfulmediasurvey.com --nginx --agree-tos --email isintex42@gmail.com
+sudo certbot -n --expand -d mindfulmediasurvey.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email isintex42@gmail.com
