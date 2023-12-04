04031169a9168094376cf0678b98478579b0cc60
Braden2n <braden@toonetown.com>
2023-12-04 15:08:53 -0700

    Certbot Fixed for mindfulmediasurvey.com

--- WebApp/.platform/hooks/postdeploy/00_get_certificate.sh
+++ WebApp/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot delete --cert-name mindfulmediasurvey.com
-sudo certbot delete --cert-name www.mindfulmediasurvey.com
-sudo certbot delete --cert-name mindfulmediasurvey.us-east-1.elasticbeanstalk.com
 sudo certbot -n --expand -d mindfulmediasurvey.com --nginx --agree-tos --email isintex42@gmail.com
