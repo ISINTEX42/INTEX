8858f9290a438dfeee3afa0a83a068517dd3984d
Braden2n <braden@toonetown.com>
2023-12-04 13:59:37 -0700

    Fix to Certbot

--- WebApp/.platform/hooks/postdeploy/00_get_certificate.sh
+++ WebApp/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d mindfulmediasurvey.com -d www.mindfulmediasurvey.com -d --nginx --agree-tos --email isintex42@gmail.com
-sudo certbot -n -d mindfulmediasurvey.us-east-1.elasticbeanstalk.com -d --nginx --agree-tos --email isintex42@gmail.com
+sudo certbot -n -d mindfulmediasurvey.com -d www.mindfulmediasurvey.com -d mindfulmediasurvey.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email isintex42@gmail.com
