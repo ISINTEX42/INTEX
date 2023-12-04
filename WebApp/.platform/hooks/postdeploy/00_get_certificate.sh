09261d510300363c2fb644862caeaa158b528894
Braden2n <braden@toonetown.com>
2023-12-04 13:36:48 -0700

    Initial Deployment

--- WebApp/.platform/hooks/postdeploy/00_get_certificate.sh
+++ WebApp/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-sudo certbot -n -d isintex.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email isintex42@gmail.com
+sudo certbot -n -d mindfulmediasurvey.com -d www.mindfulmediasurvey.com -d --nginx --agree-tos --email isintex42@gmail.com
+sudo certbot -n -d mindfulmediasurvey.us-east-1.elasticbeanstalk.com -d --nginx --agree-tos --email isintex42@gmail.com
