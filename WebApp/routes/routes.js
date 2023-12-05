ac90ae26c999d044f5f9130e0f06585da5d1931d
knhunter <120159652+youneedanemail@users.noreply.github.com>
2023-12-05 10:26:43 -0700

    creation of tableau, signUp, adminData and updates to css

--- WebApp/routes/routes.js
+++ WebApp/routes/routes.js
@@ -2,4 +2,26 @@ module.exports = (app) => {
     app.get("/", (req, res) => {
         res.render("index");
     });
+
+// link to our tableau page
+    app.get("/tableau", (req, res) => {
+        res.render("tableau");
+    });
+
+// link to survey page
+    app.get("/survey", (req, res) => {
+        res.render("survey");
+});
+
+// link to data admin page
+    app.get("/adminData", (req, res) => {
+        res.render("adminData");
+});
+
+// link to sign up page - firstName, LastName, UserName, Password, Hidden API
+app.get("/signUp", (req, res) => {
+    res.render("signUp");
+});
+
+
 };
