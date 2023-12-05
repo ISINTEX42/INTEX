ccc69ec68c872200c2afc81b47beb38db36b64cf
Braden2n <braden@toonetown.com>
2023-12-04 22:51:52 -0700

    Data Normalization

---
+++ Documents/Database/import.sql
@@ -0,0 +1,29 @@
+/*
+    Run this to recreate the raw plainsville table
+*/
+DROP TABLE IF EXISTS raw.plainsville;
+CREATE TABLE raw.plainsville(
+	raw_id SERIAL,
+	survey_timestamp TIMESTAMP,
+	age NUMERIC,
+	gender TEXT,
+	relationship_status TEXT,
+	occupation_status TEXT,
+	affiliations TEXT,
+	is_media_user TEXT,
+	platforms TEXT,
+	media_usage TEXT,
+	purpose_frequency INTEGER,
+	distraction_frequency INTEGER,
+	restless_amount INTEGER,
+	distraction_amount INTEGER,
+	worried_amount INTEGER,
+	concentration_amount INTEGER,
+	comparison_frequency INTEGER,
+	comparison_amount INTEGER,
+	validation_frequency INTEGER,
+	depression_frequency INTEGER,
+	fluctuation_frequency INTEGER,
+	sleep_frequency INTEGER,
+	PRIMARY KEY (raw_id)
+);
