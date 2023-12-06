95f2c4b6f2e3d865bc37bf6b5456e298ae3946cd
Braden2n <braden@toonetown.com>
2023-12-05 22:05:00 -0700

    Implemented submitSignUp route

--- WebApp/routes/routes.js
+++ WebApp/routes/routes.js
@@ -41,6 +41,21 @@ For each action:
     - POST for submitting an action
 */
 module.exports = (app, knex) => {
+    const bcrypt = require("bcrypt");
+    function createEmployee(username, password, first_name, last_name, city_id, is_admin) {
+        bcrypt.hash(password, 10, (err, hash) => {
+            knex("employees").insert({
+                "username": username,
+                "hash": hash,
+                "first_name": first_name,
+                "last_name": last_name,
+                "city_id": city_id,
+                "is_admin": is_admin
+            }).then(employees => {
+                return employees
+            });
+        });
+    };
     {//Public Views
     // Landing
     app.get("/", (req, res) => {
@@ -60,8 +75,9 @@ module.exports = (app, knex) => {
     });
     // Employee Signup
     app.get("/signUp", (req, res) => {
-        res.render("signUp");
-    });};
+        res.render("signUp", {"params": {"usernames": [usernames]}});
+    });
+};
     {//Employee Views
     // Landing
     app.get("/employee/index", (req, res) => {
@@ -113,6 +129,44 @@ module.exports = (app, knex) => {
         res.render("admin/account");
     });};
     {//CRUD Actions
+    app.post("/submitSignUp", (req, res) => {
+        knex.schema.createTableIfNotExists("employees", table => {
+            table.string("username", 60).primary().notNullable();
+            table.binary("hash").notNullable();
+            table.string("first_name", 30).notNullable();
+            table.string("last_name", 30).notNullable();
+            table.string("city_id", 60).notNullable();
+            table.boolean("is_admin").notNullable();
+        }).then(employees => {
+            knex("employees").then(employees => {
+                if (employees.length == 0) {
+                    let email = "admin@provocity.com";
+                    let password = "CHANGEME123!";
+                    let first_name = "DEFAULT";
+                    let last_name = "ADMIN";
+                    let city_id = "CHANGEME123!";
+                    let is_admin = true;
+                    createEmployee(email, password, first_name, last_name, city_id, is_admin);
+                };
+                let email = req.body.workEmail;
+                let password = req.body.password;
+                let first_name = req.body.employeeFirstName;
+                let last_name = req.body.employeeLastName;
+                let city_id = req.body.employeeId;
+                let is_admin = false;
+                createEmployee(email, password, first_name, last_name, city_id, is_admin);
+                res.render("/login");
+            });
+        });
+    });
+    app.post("/submitLogin", (req, res) => {
+        req.body.workEmail
+        req.body.password
+        res.render("/employee/index")
+    });
+    app.get("/testThis", (req, res) => {
+        res.send("");
+    });
     };
     {//API Actions
     };
